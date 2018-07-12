12375051d8548c64d434bd92200be5cd17b194cd
Sun Yuxi (IFAG DES SDF FW) <Yuxi.Sun@infineon.com>
2018-07-12 13:14:59 +0200

    added simple test example

--- examples/keyword_recognition/keyword_recognition.ino
+++ examples/keyword_recognition/keyword_recognition.ino
@@ -1,6 +1,5 @@
 char output_class[6][8] = {"Silence", "Unknown", "left", "right", "on", "off"};
 
-#include <MFCC.h>
 #include <I2S.h>
 #include "kws.h"
 // currently only works with recording window length of 1
@@ -8,12 +7,6 @@ int recording_win = 4;
 int averaging_window_len = 2;
 KWS *kws;
 
-#define NUM_FEATURES 10
-
-// size of the FFT to compute
-const int fftSize = 512;
-
-// MFCC mfcc(fftSize, 10, 2);
 int callbackCounter = 0;
 void callback()
 {
---
+++ examples/simple_test/simple_test.ino
@@ -0,0 +1,32 @@
+char output_class[6][8] = {"Silence", "Unknown", "left", "right", "on", "off"};
+
+#include "staticAudioInput.h"
+#include "kws.h"
+#include "audio_left/left0.h"
+
+#define AUDIO_LENGTH 16000
+int16_t audio_buffer[AUDIO_LENGTH] = WAV_LEFT;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Printing probabilities for silence, unknown, left, right, on and off");
+  staticAudioInput audioInput(audio_buffer, AUDIO_LENGTH);
+
+  KWS kws(&audioInput);
+  kws.extract_features(); //extract mfcc features
+  kws.classify();         //classify using dnn
+  float val;
+  for (int i = 0; i < 6; i++)
+  {
+    val = kws.averaged_output[i];
+    Serial.print(val / 128);
+    Serial.print("\t");
+  }
+  Serial.println();
+  int max_ind = kws.get_top_class();
+  Serial.print("Prediction: ");
+  Serial.println(output_class[max_ind]);
+}
+
+void loop() {}
